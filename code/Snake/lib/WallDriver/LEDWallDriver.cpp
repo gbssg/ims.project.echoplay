216a76faa509f24be2d47f3ad86431fe5ac15c65
tominus3 <tomnielsen2211@gmail.com>
2025-08-26 13:42:43 +0200

    Ich habe an meiner Dokumentation weitergeschrieben

--- code/Snake/lib/WallDriver/LEDWallDriver.cpp
+++ code/Snake/lib/WallDriver/LEDWallDriver.cpp
@@ -4,16 +4,16 @@
  *  \addtogroup LEDWD LED-Wall-Driver
  *  @{
  *  \brief the bare minimum to controll the Input to the LED-Wall
- * 
+ *
  *          # Theory of operation
  *          The LED Wall Driver gets the imput from the buffer array and translates it into PIN inputs.
- * 
+ *
  *          # How to use the module
  *          Use the BufferOutput with the buffer array to iniciate the transmition to the LED Wall.
- * 
+ *
  *          # Inner workings
  *          The function buffer output calls WD_PixelPush 256 times to insert all of the pixels into the LED Wall.
- */         
+ */
 
 /**
  *  \brief Sets GPIO(General Purpous Input Output) Ports as easily readable abreviations.
@@ -40,13 +40,15 @@ static void WD_PixelPush(uint8_t value);
 
 /**
  * \brief Cycles through buffer array, extracts every value and runs Pixelpush per value
- * 
+ *
  * \param buffer reads the buffer array one by one and pushes them one by one
  */
-void WD_BufferOutput(const uint8_t *buffer) {
+void WD_BufferOutput(const uint8_t *buffer)
+{
   digitalWrite(LA, LOW);
 
-  for (int i = 0;i < 256;i++) {
+  for (int i = 0; i < 256; i++)
+  {
     uint8_t pixel = buffer[i];
     WD_PixelPush(pixel);
   }
@@ -57,11 +59,12 @@ void WD_BufferOutput(const uint8_t *buffer) {
 
 /**
  * \brief Uses digitalWrite (Arduino.h's Funktion) to input signals into Display
- * 
+ *
  * \param[in] value buffer values either on or off.
  */
-static void WD_PixelPush(uint8_t value) {
-                // one-line if statment.
+static void WD_PixelPush(uint8_t value)
+{
+  // one-line if statment.
   digitalWrite(SDI, value > 0 ? HIGH : LOW);
   delayMicroseconds(1);
   digitalWrite(CLK, HIGH);
