0f895f3842610dd621a88c77c4b4504957dda838
tominus3 <tomnielsen2211@gmail.com>
2025-12-04 11:29:34 +0100

    Small changes to Screen class

--- code/Snake/lib/Screen/Screen.cpp
+++ code/Snake/lib/Screen/Screen.cpp
@@ -15,39 +15,48 @@ Screen::Screen(uint8_t translationTable[16][16])
 }
 
 // Malt ein das image gleich wie das newImage
-void Screen::drawImage(uint8_t image[16][16], uint8_t newImage[16][16])
+void Screen::drawImage(uint8_t newImage[16][16])
 {
     for (uint8_t i = 0; i < 16; i++)
     {
         for (uint8_t j = 0; j < 16; j++)
         {
-            image[i][j] = newImage[i][j];
+            _image[i][j] = newImage[i][j];
         }
     }
 }
 
 // Macht ein gewähltes Bild zu nur nullen
-void Screen::emptyScreen(uint8_t image[16][16])
+void Screen::emptyScreen()
 {
     for (uint8_t i = 0; i < 16; i++)
     {
         for (uint8_t j = 0; j < 16; j++)
         {
-            image[i][j] = 0;
+            _image[i][j] = 0;
         }
     }
 }
 
-// Macht ein gewoltes Bild in den Buffer und dieser wird auf den Screen gemalt und angezeigt
-void Screen::updateScreen(uint8_t image[16][16], uint8_t buffer[256])
+// Empties the buffer
+void Screen::emptyBuffer()
 {
-    LWC_Encode(buffer, image, _translationTable);
+    for (int i = 0; i < 256; i++)
+    {
+        _buffer[i] = 0;
+    }
+}
 
-    WD_BufferOutput(buffer);
+// Macht ein gewoltes Bild in den Buffer und dieser wird auf den Screen gemalt und angezeigt
+void Screen::update()
+{
+    LWC_Encode(_buffer, _image, _translationTable);
+
+    WD_BufferOutput(_buffer);
 }
 
 // Ein Funktion von der Glyph Library um den jetzigen zustand vom Screen zu malen
-void Screen::imagePrint(uint8_t image[16][16])
+void Screen::imagePrint()
 {
     Serial.println("+----------------------------------+");
     for (int y = 0; y < 16; y++)
@@ -55,9 +64,15 @@ void Screen::imagePrint(uint8_t image[16][16])
         Serial.print("| ");
         for (int x = 0; x < 16; x++)
         {
-            Serial.print(image[y][x] > 0 ? "XX" : "  ");
+            Serial.print(_image[y][x] > 0 ? "XX" : "  ");
         }
         Serial.println(" |");
     }
     Serial.println("+----------------------------------+");
+}
+
+// Setzt ein Pixel auf dem image (lightOn bestimmt ob an oder aus)
+void Screen::setPixel(int x, int y, bool lightOn)
+{
+    _image[y][x] = lightOn ? 1 : 0;
 }
--- code/Snake/lib/Screen/Screen.h
+++ code/Snake/lib/Screen/Screen.h
@@ -5,16 +5,17 @@
 class Screen
 {
 public:
-    uint8_t _image[16][16] = {0};
-    uint8_t _buffer[256] = {0};
-
     Screen(uint8_t translationTable[16][16]);
 
-    void drawImage(uint8_t image[16][16], uint8_t newImage[16][16]);
-    void emptyScreen(uint8_t image[16][16]);
-    void updateScreen(uint8_t image[16][16], uint8_t buffer[256]);
-    void imagePrint(uint8_t image[16][16]);
+    void drawImage(uint8_t newImage[16][16]);
+    void emptyScreen();
+    void emptyBuffer();
+    void update();
+    void imagePrint();
+    void setPixel(int x, int y, bool lightOn);
 
 private:
+    uint8_t _image[16][16] = {0};
+    uint8_t _buffer[256] = {0};
     uint8_t _translationTable[16][16];
 };
