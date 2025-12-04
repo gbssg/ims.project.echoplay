dbb3345a093ceb06e0a1103b4f3fef91c9f1f650
tominus3 <tomnielsen2211@gmail.com>
2025-12-04 11:30:20 +0100

    Add CarJumpStrategy class implementing IGame interface
    - Introduced CarJumpStrategy class to manage the game logic for the Car Jump feature.
    - Added setup, start, update, and end methods to handle game lifecycle.
    - Included necessary headers for screen management and button input.

--- code/Snake/lib/Car-Jump/Car-Jump.h
+++ code/Snake/lib/Car-Jump/Car-Jump.h
@@ -2,3 +2,20 @@
 
 #include <stdint.h>
 #include <SparkFun_Qwiic_Button.h>
+#include <Screen.h>
+#include <IGame.h>
+#include <SimpleSoftTimer.h>
+
+using namespace HolisticSolutions;
+
+class CarJumpStrategy : public IGame
+{
+public:
+    void setup(Screen &screen, QwiicButton &leftButton, QwiicButton &rightButton);
+
+    void start(Screen &screen, QwiicButton &leftButton, QwiicButton &rightButton);
+
+    void update(Screen &screen, QwiicButton &leftButton, QwiicButton &rightButton);
+
+    void end(Screen &screen, QwiicButton &leftButton, QwiicButton &rightButton);
+};
